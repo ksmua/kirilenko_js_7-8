8a5b24bdea9d10742ae8ac7ae114e6bd00b346f9
Sergii <ksmua85@gmail.com>
2017-03-14 01:20:17 +0200

    tooltips no work

--- js/7-8.js
+++ js/7-8.js
@@ -4,8 +4,50 @@
 //         }
 // });
 $(document).ready(function () {
-	console.log("ready");
-	// $('.curent').show();
+	// var input = $('input');
+	// console.log(input);
+	// function getInputValue (){
+	// 	return($(this).attr(title));
+	// }
+	function addToolTip(){
+		$(this).after('<span class="tooltip">' + $(this).attr('title') + '</span>');
+	}
+	// var inputValue = $('input').attr("title");
+	// $('input').after('<span class="tooltip">' + inputValue + '</span>');
+	
+	$('input').bind("focus", function(){
+		addToolTip(this);
+		$(this).next().show(500);
+	});
+	
+	$('input').bind("blur", function(){
+		$(this).next().hide(500);
+	});
+
+	$('input').bind("mouseover", function(){
+		$(this).next().show(500);
+	});
+
+	$('input').bind("mouseout", function(){
+		$(this).next().hide(500);
+	});
+
+	// $('input').bind("focus", function(){
+	//  	var value = $(this).attr("title");
+	//  	console.log(value);
+	//  	$(this).after('<span class="tooltip">' + value + '</span>');
+	//  });
+	// $('input').bind("blur", function(){
+
+	
+
+	// .mouseenter(function() {
+ //    $('#follow').css('display','block');
+	// })
+	// .mouseleave(function() {
+ //    $('#follow').css('display','none');
+	// })
+
 	$('li').click(function(){
 		if( $(this).hasClass('curent-tab') ){
 			console.log('already curent');
